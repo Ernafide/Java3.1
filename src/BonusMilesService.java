f29a7c191231c5f1ec34f65139d06fe7aaa9b4b1
Julia <Ernafide@gmail.com>
2022-10-20 16:58:45 +0300

    Origin

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int rudleMile = 20;
+        int miles;
+        miles = cost / rudleMile;
+        return miles;
+    }
+
+}
